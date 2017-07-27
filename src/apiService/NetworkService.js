5c0400da0b67debad2d980a3b454837763d22c88
kdichev <konstantin.dichev@gmail.com>
2017-07-27 16:17:14 +0200

    clean up

--- src/apiService/NetworkService.js
+++ src/apiService/NetworkService.js
@@ -3,7 +3,6 @@ import RequestService from './RequestService'
 const BASE_URL = "http://52.18.171.117/api/kiosk"
 
 class NetworkService {
-
   getLocation() {
     const url = `${BASE_URL}/location/338495a7-d32c-4991-bc32-74018850497b`
     return RequestService.request(url, 'GET')
@@ -23,7 +22,6 @@ class NetworkService {
     const url = `${BASE_URL}/reservation/create`
     return RequestService.request(url, 'POST', data)
   }
-
 }
 
 export default new NetworkService()
